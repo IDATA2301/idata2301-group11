40307293b58dff37dd1e974b7a8a98ab5b7c646b
Sebastian <sebastian.lid.nesvik@gmail.com>
2026-02-05 13:39:52 +0100

    Created header

--- roamroute/src/App.tsx
+++ roamroute/src/App.tsx
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "./App.css"
+
+import Header from "./components/header/header.tsx"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav style={{ padding: '20px', borderBottom: '1px solid #ccc' }}>
-          <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-          <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-          <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
-        </nav>
+      <Header />
 
-        <div style={{ padding: '20px' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
+      <div style={{ padding: "20px", marginTop: "80px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </Router>
   )
