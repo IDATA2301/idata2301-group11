391089566f24aca83328a592acb17424e9f5a3d0
Sebastian <sebastian.lid.nesvik@gmail.com>
2026-02-12 12:51:06 +0100

    Fixed loginpage styling, aswell as changed background color again to correct color.

--- roamroute/src/pages/LoginPage.tsx
+++ roamroute/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import "./styling/LoginPage.css"
+import { EnvelopeIcon, LockClosedIcon } from "@heroicons/react/24/outline"
 
 export default function LoginPage() {
   return (
@@ -10,22 +11,39 @@ export default function LoginPage() {
 
       {/* Login form */}
       <form className="login-form">
-        <input 
-        type="text" 
-        placeholder="name@example.com" />
-        <input 
-        type="password" 
-        placeholder="*********" />
+        <label className="email-label" htmlFor="login-email">
+          Email address
+        </label>
+        <div className="input-with-icon">
+          <EnvelopeIcon className="login-icon" aria-hidden="true" />
+          <input 
+            id="login-email"
+            type="email" 
+            placeholder="name@example.com"
+          />
+        </div>
+        <label className="password-label" htmlFor="login-password">
+          Password
+        </label>
+        <div className="input-with-icon">
+          <LockClosedIcon className="login-icon" aria-hidden="true" />
+          <input 
+            id="login-password"
+            type="password" 
+            placeholder="*********"
+          />
+        </div>
+        <div className="forgot-password">
+          <a href="/forgot-password">Forgot password?</a>
+        </div>
         <button type="submit">Sign In</button>
       </form>
 
       {/* Additional links */}
-      <div className="forgot-password">
-        <a href="/forgot-password">Forgot password?</a>
-      </div>
-
       <div className="signup-link">
-        <p>Don't have an account? <a href="/signup">Sign Up</a></p>
+        <p>
+          Don&apos;t have an account? <a href="/signup">Sign Up</a>
+        </p>
       </div>
     </>
   )
